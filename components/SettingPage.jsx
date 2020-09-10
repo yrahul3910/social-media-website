709e2b7e5c00ef6a971726cbb88f81c4d9a1922e
Shuzheng <shuzheng@Shuzhengs-MBP.attlocal.net>
2020-09-09 21:17:59 -0400

    add upload button

--- components/SettingPage.jsx
+++ components/SettingPage.jsx
@@ -15,7 +15,12 @@ export default class SettingPage extends React.Component {
         }
         this.onCrop = this.onCrop.bind(this);
         this.onClose = this.onClose.bind(this);
+        this.click = this.click.bind(this);
+        this.click2 = this.click2.bind(this)
         this.handleChange = this.handleChange.bind(this);
+
+		this.password = React.createRef();
+        this.setState.preview = React.createRef();
     }
 
     onClose() {
@@ -30,32 +35,75 @@ export default class SettingPage extends React.Component {
     	this.setState({ checked });
     }
 
+	async click2() {
+        const response = await fetch('http://localhost:8000/api/authenticate', {
+            method: "PUT",
+            mode: "cors",
+            cache: "no-cache",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                password: this.password.current.value
+            })
+        });
+	}
+    async click() {
+        const response = await fetch('http://localhost:8000/api/profileImage', {
+            method: "POST",
+            mode: "cors",
+            cache: "no-cache",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                profileImage: this.state.preview
+            })
+        });
+
+        const data = response.json();
+
+        if (!data.success)
+            this.message.current.innerHTML = `<span style='color: red'>${data.message}</span>`;
+        else {
+            this.message.current.innerHTML = `<span style='color: green'>${data.message}</span>`;
+
+            localStorage.setItem('token', data.token);
+            this.props.toggleLogin(data.user);
+            this.setState({loggedIn: true});
+        }
+    }
+
     render() {
         return (
 			<div>
 				<Navbar dp={this.props.user ? this.props.user.dp : 'http://localhost:8000/account_circle.png'} />
                 <div className="row" style={{marginTop: '70px'}}>
-                	<div className="col-4 col-gap-8">
-                	<h2> Upload your profile image </h2>
-			        <Avatar
-                      width={300}
-                      height={200}
-                      onCrop={this.onCrop}
-                      onClose={this.onClose}
-                    />
-                    <img class = "galimage" src={this.state.preview} alt="Preview" />
+                	<div className="col-4 col-gap-8" >
+                		<h2> Upload your profile image </h2>
+			        	<Avatar
+                      		width={300}
+                      		height={200}
+                      		onCrop={this.onCrop}
+                      		onClose={this.onClose}
+                    	/>
+                    	<img class = "galimage" src={this.state.preview} alt="Preview" />
+                    	<button className="fill" onClick={this.click} style={{width: '25%'}}> Upload </button>
                     </div>
-                	<div className="col-6 col-gap-9">
+                	<div className="col-4 col-gap-9" >
                 		<h2> Make your profile public? </h2>
-                		<div class="row">
+                		<div class="row" style={{marginTop: '5px'}}>
                 			<Switch onChange={this.handleChange} checked={this.state.checked} />
                 		</div>
-                		<h2> change your password </h2>
-                		<div class="row">
-                			<input type="password" style={{width: '15vw'}} placeholder="old password, min 8 character" />
+                		<h2> Change your password </h2>
+                		<div class="row" style={{marginTop: '5px'}}>
+                			<input type="password" style={{width: '16vw'}} placeholder="old password, min 8 character" />
                 		</div>
-                		<div class="row">
-                			<input type="password" style={{width: '15vw'}} placeholder="new password, min 8 character" />
+                		<div class="row" style={{marginTop: '5px'}}>
+                			<input type="password" style={{width: '16vw'}} placeholder="new password, min 8 character" />
+                		</div>
+                		<div class="row" style={{marginTop: '5px'}}>
+                			<button className="fill" onClick={this.click2} style={{width: '25%'}}> Update </button>
                 		</div>
                     </div>
                 </div>
