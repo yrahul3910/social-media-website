88fc2d77b1893a73d995a6cdf80e4cdd25c4fb22
Rahul Yedida <yrahul3910@gmail.com>
2020-09-10 23:02:25 -0400

    Final lint fixes

--- server/db.js
+++ server/db.js
@@ -1,4 +1,4 @@
-import {MongoClient} from 'mongodb';
+import { MongoClient } from 'mongodb';
 import dotenv from 'dotenv';
 import bcrypt from 'bcrypt';
 
@@ -15,15 +15,18 @@ const uri = `mongodb+srv://${user}:${password}@${hostname}/db?retryWrites=true&w
  * @param {string} pwd - The plaintext password entered
  * @param {Function} func - The callback function
  */
-exports.authenticate = (username, password, callback) => {
+exports.authenticate = (username, pwd, callback) => {
     const client = new MongoClient(uri, { useNewUrlParser: true });
     client.connect(err => {
         if (err) throw err;
 
         const collection = client.db('db').collection('users');
-        collection.findOne({username}, (err, results) => {
-            if (err) {
-                callback({success: false, message: 'Find failed.'});
+        collection.findOne({ username }, (findErr, results) => {
+            if (findErr) {
+                callback({
+                    success: false,
+                    message: 'Find failed.'
+                });
                 return;
             }
 
@@ -37,13 +40,19 @@ exports.authenticate = (username, password, callback) => {
             }
 
             // Check password
-            bcrypt.compare(password, results.password, (e, res) => {
+            bcrypt.compare(pwd, results.password, (e, res) => {
                 if (e || !res) {
-                    callback({success: false, message: 'Comparison failed'});
+                    callback({
+                        success: false,
+                        message: 'Comparison failed'
+                    });
                     return;
                 }
-                
-                callback(null, {success: true, results});
+
+                callback(null, {
+                    success: true,
+                    results
+                });
             });
         });
 
@@ -55,44 +64,60 @@ exports.authenticate = (username, password, callback) => {
  * Registers a user by adding the details to the users table. Also adds user to
  * subscriptions table, since by default, every user subscribes to him/herself.
  * @param {string} username - The username of the new user
- * @param {string} pwd - The plaintext password of the user. This will be hashed.
+ * @param {string} pwd - The plaintext password of a user. This will be hashed.
  * @param {string} name - The user's name
  * @param {Function} func - The callback function
  */
-exports.register = (username, password, name, callback) => {
+exports.register = (username, pwd, name, callback) => {
     console.log(uri);
     const client = new MongoClient(uri, { useNewUrlParser: true });
     client.connect(err => {
         if (err) throw err;
 
         const collection = client.db('db').collection('users');
-        collection.findOne({username}, (err, results) => {
-            if (err) {
-                callback({success: false, message: 'Could not find user.'});
+        collection.findOne({ username }, (e, results) => {
+            if (e) {
+                callback({
+                    success: false,
+                    message: 'Could not find user.'
+                });
                 return;
             }
 
             // If username exists, error
             if (results) {
-                callback({success: false, message: 'Username exists.'});
+                callback({
+                    success: false,
+                    message: 'Username exists.'
+                });
                 return;
             }
 
             // Hash the password
-            bcrypt.hash(password, 10, (e, hash) => {
-                if (e) {
-                    callback({success: false, message: 'Failed to hash password.'});
+            bcrypt.hash(pwd, 10, (hashErr, hash) => {
+                if (hashErr) {
+                    callback({
+                        success: false,
+                        message: 'Failed to hash password.'
+                    });
                     return;
                 }
 
                 // Insert into the database
-                collection.insertOne({username, password: hash, name}, e_ => {
+                collection.insertOne({
+                    username,
+                    password: hash,
+                    name
+                }, e_ => {
                     if (e_) {
-                        callback({success: false, message: 'Failed to insert'});
+                        callback({
+                            success: false,
+                            message: 'Failed to insert'
+                        });
                         return;
                     }
 
-                    callback(null, {success: true});
+                    callback(null, { success: true });
                 });
             });
         });
@@ -100,18 +125,13 @@ exports.register = (username, password, name, callback) => {
     client.close();
 };
 
-exports.getFriendPosts = async (username, callback) => {
+exports.getFriendPosts = async(username, callback) => {
     const client = new MongoClient(uri, { useNewUrlParser: true });
     client.connect(err => {
         if (err) throw err;
 
-        let collection = client.db('db').collection('friends');
-        collection.find({
-            $or: [
-                {'user1': username},
-                {'user2': username}
-            ]
-        }, async (e, friends) => {
+        const collection = client.db('db').collection('friends');
+        collection.find({ $or: [{ user1: username }, { user2: username }]}, async(e, friends) => {
             if (e) {
                 callback({
                     success: false,
@@ -120,15 +140,15 @@ exports.getFriendPosts = async (username, callback) => {
                 return;
             }
 
-            let f = await friends.toArray();
-            let post_coll = client.db('db').collection('posts');
+            const f = await friends.toArray();
+            const post_coll = client.db('db').collection('posts');
             post_coll.find({
-                'username': {
+                username: {
                     $in: f.map(x => x.user1).concat(
                         f.map(x => x.user2))
                 }
-            }, async (e, posts) => {
-                if (e) {
+            }, async(postErr, posts) => {
+                if (postErr) {
                     callback({
                         success: false,
                         message: 'Could not find posts'
@@ -136,7 +156,7 @@ exports.getFriendPosts = async (username, callback) => {
                     return;
                 }
 
-                let postsArr = await posts.toArray();
+                const postsArr = await posts.toArray();
                 console.log(postsArr);
 
                 callback(null, postsArr);
