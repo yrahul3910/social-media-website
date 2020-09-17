29175a3051d15132c01b50cd6a1e3e0435521943
Shuzheng <shuzheng@Shuzhengs-MBP.attlocal.net>
2020-09-17 02:43:03 -0400

    try to make the profile image uploader work

--- components/SettingPage.jsx
+++ components/SettingPage.jsx
@@ -4,12 +4,13 @@ import Navbar from './Navbar.jsx';
 import Avatar from 'react-avatar-edit';
 import Switch from 'react-switch';
 import { useAlert } from 'react-alert';
-
+const owasp = require("owasp-password-strength-test");
 
 class SettingPage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+        	loggedIn: true,
             preview: null,
             checked: false
         };
@@ -17,6 +18,11 @@ class SettingPage extends React.Component {
         this.onCrop = this.onCrop.bind(this);
         this.onClose = this.onClose.bind(this);
         this.handleChange = this.handleChange.bind(this);
+
+        this.onUpdateClick = this.onUpdateClick.bind(this);
+        this.onUploadClick = this.onUploadClick.bind(this);
+        this.onDeleteClick = this.onDeleteClick.bind(this);
+        this.onDownloadClick = this.onDownloadClick.bind(this);
 
         this.password = React.createRef();
         this.message = React.createRef();
@@ -36,6 +42,7 @@ class SettingPage extends React.Component {
     }
 
     async onUpdateClick() {
+
         const response = await fetch('http://localhost:8000/api/authenticate', {
             method: 'PUT',
             mode: 'cors',
@@ -68,7 +75,39 @@ class SettingPage extends React.Component {
         else {alert.success('Success!');}
     }
 
+    async onDeleteClick() {
+        const alert = useAlert();
+        const response = await fetch('http://localhost:8000/api/user', {
+            method: 'POST',
+            mode: 'cors',
+            cache: 'no-cache',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ profileImage: this.state.preview })
+        });
+
+        const body = await response.json();
+        if (!body.success) {alert.error('Failed to delete the account.');}
+        else {alert.success('Done, see you again!');}
+    }
+
+    async onDownloadClick() {
+        const alert = useAlert();
+        const response = await fetch('http://localhost:8000/api/dataDownload', {
+            method: 'POST',
+            mode: 'cors',
+            cache: 'no-cache',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ profileImage: this.state.preview })
+        });
+
+        const body = await response.json();
+        if (!body.success) {alert.error('Failed to download the data.');}
+        else {alert.success('download Success!');}
+    }
+
     render() {
+    	if (!this.state.loggedIn) {return <Redirect to='/login' />;}
+
         return (
             <div>
                 <Navbar dp={this.props.user ?
@@ -88,21 +127,29 @@ class SettingPage extends React.Component {
                     </div>
                     <div className="col-4 col-gap-9" >
                         <h2> Make your profile public? </h2>
-                        <div className="row">
+                        <div className="row" style={{ marginTop: '10px' }}>
                             <Switch onChange={this.handleChange} checked={this.state.checked} />
                         </div>
                         <h2> Change your password </h2>
                         <div className="row" ref={this.message}></div>
-                        <div className="row" style={{ marginTop: '5px' }}>
+                        <div className="row" style={{ marginTop: '10px' }}>
                             <input type="password" style={{ width: '16vw' }} placeholder="old password" />
                         </div>
-                        <div className="row" style={{ marginTop: '5px' }}>
+                        <div className="row" style={{ marginTop: '10px' }}>
                             <input type="password" ref={this.password} style={{ width: '16vw' }}
                                 placeholder="new password" />
                         </div>
-                        <div className="row" style={{ marginTop: '5px' }}>
+                        <div className="row" style={{ marginTop: '10px' }}>
                             <button className="fill" onClick={this.onUpdateClick}
-                                style={{ width: '25%' }}> Update </button>
+                                style={{ width: '50%' }}> Update Password</button>
+                        </div>
+                        <div className="row" style={{ marginTop: '10px' }}>
+                            <button className="fill" onClick={this.onDeleteClick}
+                                style={{ width: '50%' }}> delete account </button>
+                        </div>
+                        <div className="row" style={{ marginTop: '10px' }}>
+                            <button className="fill" onClick={this.onDownloadClick}
+                                style={{ width: '50%' }}> download personal data </button>
                         </div>
                     </div>
                 </div>
